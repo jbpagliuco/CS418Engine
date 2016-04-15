8cf74c8b376b1cf64d00263d13ad55b5e6c5c090
Joe Pagliuco <jbpagliuco@comcast.net>
2016-04-15 00:01:46 -0500

    Screenshot of depth buffer

--- src/graphics/Renderer.cpp
+++ src/graphics/Renderer.cpp
@@ -12,6 +12,8 @@
 #include "util/ColorDefs.h"
 #include "graphics/BasicMeshes.h"
 
+#include <fstream>
+
 namespace CS418
 {
 	Renderer::~Renderer()
@@ -118,6 +120,25 @@ namespace CS418
 				glDrawElements(GL_TRIANGLES, m_postRC.m_indicesCount, GL_UNSIGNED_INT, 0);
 
 				glEnable(GL_DEPTH_TEST);
+
+				int x = 1920;
+				int y = 1080;
+				long imageSize = x * y * 3;
+				unsigned char *data = new unsigned char[imageSize];
+				glReadPixels(0, 0, x, y, GL_BGR, GL_UNSIGNED_BYTE, data);// split x and y sizes into bytes
+				glGetTextureImage(m_post.GetDepthMap(), 0.0f, GL_BGR, GL_UNSIGNED_BYTE, imageSize, data);
+				int xa = x % 256;
+				int xb = (x - xa) / 256;int ya = y % 256;
+				int yb = (y - ya) / 256;//assemble the header
+				unsigned char header[18] = { 0,0,2,0,0,0,0,0,0,0,0,0,(char)xa,(char)xb,(char)ya,(char)yb,24,0 };
+				// write header and data to file
+				std::fstream File("test.tga", std::ios::out | std::ios::binary);
+				File.write(reinterpret_cast<char *>(header), sizeof(char) * 18);
+				File.write(reinterpret_cast<char *>(data), sizeof(char)*imageSize);
+				File.close();
+
+				delete[] data;
+				data = NULL;
 			}
 		}
 	}
